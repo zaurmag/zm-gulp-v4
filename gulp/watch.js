05058a594c50c1301ef8aaea4c7c4fd0c3c9ff63
Zaur <zaurmag411@gmail.com>
2018-08-16 20:26:18 +0300

    Add spriteSvg watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -6,5 +6,6 @@ module.exports = function () {
         $.gulp.watch(path.path.src.js, $.gulp.parallel('js:build'));
         $.gulp.watch(path.path.src.img, $.gulp.parallel('img:build'));
         $.gulp.watch(path.path.src.pngIcons, $.gulp.parallel('spriteImg:build'));
+        $.gulp.watch(path.path.src.svgIcons, $.gulp.parallel('spriteSvg:build'));
     });
 };
