c7e44612159127342b9a5d9866c6a7f4bca128db
Zaur <zaurmag411@gmail.com>
2018-08-16 21:32:15 +0300

    Fix bug

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,7 +1,7 @@
 var path = require('./path/path.js');
 module.exports = function () {
     $.gulp.task('watch', function() {
-        $.gulp.watch(path.path.src.pug, $.gulp.parallel('pug:build'));
+        $.gulp.watch(path.path.watch.pug, $.gulp.parallel('pug:build'));
         $.gulp.watch(path.path.watch.style, $.gulp.parallel('style:build', 'styleDev:build'));
         $.gulp.watch(path.path.src.js, $.gulp.parallel('js:build'));
         $.gulp.watch(path.path.src.img, $.gulp.parallel('img:build'));
