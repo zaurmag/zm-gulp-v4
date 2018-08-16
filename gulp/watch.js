501e9207d7caa90627f6ab040eb774c313369199
Zaur <zaurmag411@gmail.com>
2018-08-16 20:17:44 +0300

    Add styleDev:build task

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,7 +2,7 @@ var path = require('./path/path.js');
 module.exports = function () {
     $.gulp.task('watch', function() {
         $.gulp.watch(path.path.src.pug, $.gulp.parallel('pug:build'));
-        $.gulp.watch(path.path.watch.style, $.gulp.parallel('style:build'));
+        $.gulp.watch(path.path.watch.style, $.gulp.parallel('style:build', 'styleDev:build'));
         $.gulp.watch(path.path.src.js, $.gulp.parallel('js:build'));
         $.gulp.watch(path.path.src.img, $.gulp.parallel('img:build'));
         $.gulp.watch(path.path.src.pngIcons, $.gulp.parallel('spriteImg:build'));
