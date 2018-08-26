396f668b440f9f009107d3dc78fb82d0afc643d1
Zaur <zaurmag411@gmail.com>
2018-08-26 20:01:36 +0300

    I combed my code

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,4 +1,5 @@
-// ======= Tabs Function =======
+/* Tabs Function
+   ========================================================================= */
 
 (function($) {
     $.fn.tabs = function() {
@@ -14,47 +15,71 @@
 
 })(jQuery);
 
-// ======= Ajax Submit Form Plugin =======
+/* Ajax SimpleSubmit Form Plugin
+   ========================================================================= */
 (function($) {
-    jQuery.fn.sendForm = function(options) {
+    $.fn.simpleSendForm = function(options) {
+        // Options
         options = $.extend({
             successTitle: "Спасибо, что выбрали нас!",
             successText: "Мы свяжемся с Вами в ближайшее время.",
             errorTitle: "Сообщение не отправлено!",
             errorSubmit: "Ошибка отправки формы!",
-            errorNocaptcha: "Вы не прошли проверку каптчи",
-            errorCaptcha: "Вы не заполнили каптчу",
+            errorNocaptcha: "Вы не заполнили каптчу",
+            errorCaptcha: "Вы не прошли проверку каптчи",
             mailUrl: "../form-submit/submit.php",
             autoClose: false,
-            autoCloseDelay: 5000
+            autoCloseDelay: 5000,
+            debug: false,
+            captcha: false,
+            captchaPublicKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
         }, options);
 
+        if(options.captcha) {
+            window.onload = function () {
+                var addScriptCaptcha = document.createElement('script');
+                addScriptCaptcha.src = 'https://www.google.com/recaptcha/api.js';
+                document.body.appendChild(addScriptCaptcha);
+            };
+        }
+
+        // Submit function
         var make = function() {
-            var $this = $(this);
+            var $this = $(this),
+                form = $this.find('.form'),
+                btn = $this.find('.btn-submit'),
+                captcha = $this.find('.recaptcha');
+
+            if(options.captcha) {
+                captcha.append('<div class="g-recaptcha" data-sitekey="' + options.captchaPublicKey + '"></div>')
+            }
+
             $(this).submit(function() {
                 var data = $(this).serialize();
+                function errorRes(errorMessage) {
+                    btn.removeClass('progress-bar-animated progress-bar-striped bg-success');
+                    $this.append('<div class="form__error alert alert-danger text-center mt-3 mb-0">' + errorMessage + '</div>');
+                    setTimeout(function() {
+                        $this.find('.form__error').remove();
+                    }, 5000);
+                }
                 $.ajax({
                     url: options.mailUrl,
                     type: "POST",
                     data: data,
                     beforeSend: function() {
-                        $this.find('.btn-submit').parents('.form__form').addClass('sending');
+                        btn.addClass('progress-bar-animated progress-bar-striped bg-success');
                     },
                     success: function(res) {
-                        function errorRes(errorMessage) {
-                            $this.find('.btn-submit').parents('.form__form').removeClass('sending');
-                            $this.append('<div class="form__error">' + errorMessage + '</div>');
-                            setTimeout(function() {
-                                $this.find('.form__error').remove();
-                            }, 5000);
-                        }
                         if (res == 1) {
                             $this[0].reset();
-							grecaptcha.reset();
+                            if(options.captcha) {
+                                grecaptcha.reset();
+                            }
                             $this.find('.form__hide-success').slideUp().delay(5000).slideDown();
-                            $this.find('.btn-submit').parents('.form__form').removeClass('sending');
-                            $this.find('.form__hide-success').after('<div class="form__sys-message"></div>');
-                            $this.find('.form__sys-message').html('<div class="form__success-title">' + options.successTitle + '</div><p class = "form__success-text" >' + options.successText + '</p>');
+                            btn.removeClass('progress-bar-animated progress-bar-striped bg-success');
+                            $this.find('.form__hide-success').after('<div class="form__sys-message alert alert-success text-center mb-0"></div>');
+                            $this.find('.form__sys-message').html('<h4 class="form__success-title alert-heading">' + options.successTitle + '</h4><p class = "form__success-text" >' + options.successText + '</p>');
                             setTimeout(function() {
                                 $this.find('.form__sys-message').fadeOut().delay(3000).remove();
                                 if (options.autoClose) {
@@ -68,15 +93,20 @@
                         } else {
                             errorRes(options.errorSubmit);
                         }
+                        if(options.debug) {
+                            console.log(res);
+                        }
                     },
-                    error: function() {
+                    error: function(res) {
                         errorRes(options.errorSubmit);
+                        if(options.debug) {
+                            console.log(res);
+                        }
                     }
                 });
                 return false;
             });
-
-        }; // end make
+        };
 
         return this.each(make);
     };
@@ -129,7 +159,6 @@
                     dropdownFn(btnDropdown);
                 else
                     dropdownFn($(parentA));
-
             }
         };
         if (options.destroy) {
@@ -140,7 +169,8 @@
     };
 })(jQuery);
 
-// ======= Go Top =======
+/* Go top
+   ========================================================================= */
 
 (function($) {
     jQuery.fn.scrollToTop = function() {
@@ -164,7 +194,8 @@
     }
 })(jQuery);
 
-// ======= Toggle text function =======
+/* Toggle text function 
+   ========================================================================= */
 (function($) {
     jQuery.fn.extend({
         toggleText: function(stateOne, stateTwo) {
